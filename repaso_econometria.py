df47d668b4ef909b1d654ffdd6f94c4dd0d54070
RichardBCH <125785369+RichardBCH@users.noreply.github.com>
2023-02-19 01:50:19 -0500

    Funciones

--- repaso_econometria.py
+++ repaso_econometria.py
@@ -1,1 +1,7 @@
-Hola mundo
+#Funciones
+def mi_funcion(param1, param2):
+    """Esta funcion imprime los dos valores pasados como parametros"""
+    print(param1)
+    print(param2)
+mi_funcion("Hola", 2)
+##mi_funcion(param2=2, param1="Hola mundo")
